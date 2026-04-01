b275a25cfbf2ae58b45d05e19f724e9f4aae75c7
mochinekoo <mochinekoo0113@gmail.com>
2026-04-01 21:31:14 +0900

    [add] 地震情報（仮）を出せるようになりました

--- MainEarthquakeWindow.xaml.cs
+++ MainEarthquakeWindow.xaml.cs
@@ -18,6 +18,64 @@ namespace YMM4_Earthquake_Plugin {
     public partial class MainEarthquakeWindow : UserControl {
         public MainEarthquakeWindow() {
             InitializeComponent();
+            InitWindow();
+        }
+
+        public void InitWindow() {
+            foreach (var api in P2PEarthquakeAPI.GetInstance()) {
+                var earthquake = api.earthquake;
+                var hypocenter = earthquake.hypocenter;
+                var stackPanel = CreateEarthquakeGrid(earthquake.time, hypocenter.name, hypocenter.magnitude, earthquake.maxScale);
+                EarthquakeListStack.Items.Add(stackPanel);
+            }
+        }
+
+        private Grid CreateEarthquakeGrid(string time, string name, float magnitude, int maxScale) {
+            Grid grid = new Grid();
+            grid.HorizontalAlignment = HorizontalAlignment.Stretch;
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.Background = convertColor(maxScale);
+
+            TextBlock textBox = new TextBlock();
+            textBox.Text = time + "　" + name;
+            Grid.SetRow(textBox, 0);
+            grid.Children.Add(textBox);
+            TextBlock magnitudeText = new TextBlock();
+            magnitudeText.Text = magnitude.ToString();
+            Grid.SetRow(magnitudeText, 1);
+            grid.Children.Add(magnitudeText);
+
+            return grid;
+        }
+
+        public string convertScale(int maxScale) {
+            if (maxScale == 10) return "震度1";
+            else if (maxScale == 20) return "震度2";
+            else if (maxScale == 30) return "震度3";
+            else if (maxScale == 40) return "震度4";
+            else if (maxScale == 45) return "震度5弱";
+            else if (maxScale == 50) return "震度5強";
+            else if (maxScale == 55) return "震度6弱";
+            else if (maxScale == 60) return "震度6強";
+            else if (maxScale == 65) return "震度7";
+            return "不明";
+        }
+
+        public SolidColorBrush convertColor(int maxScale)
+        {
+            if (maxScale == 10) return new SolidColorBrush(Colors.White);
+            else if (maxScale == 20) return new SolidColorBrush(Colors.AliceBlue);
+            else if (maxScale == 30) return new SolidColorBrush(Colors.Green);
+            else if (maxScale == 40) return new SolidColorBrush(Colors.Yellow);
+            else if (maxScale == 45) return new SolidColorBrush(Colors.Orange);
+            else if (maxScale == 50) return new SolidColorBrush(Colors.DarkOrange);
+            else if (maxScale == 55) return new SolidColorBrush(Colors.Red);
+            else if (maxScale == 60) return new SolidColorBrush(Colors.DarkRed);
+            else if (maxScale == 65) return new SolidColorBrush(Colors.Purple);
+            return new SolidColorBrush(Colors.White);
         }
     }
 }
---
+++ P2PEarthquakeAPI.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Transactions;
+
+namespace YMM4_Earthquake_Plugin {
+    internal class P2PEarthquakeAPI {
+
+        public static List<P2PEarthquakeAPI>? GetInstance() {
+            using (HttpClient client = new HttpClient()) {
+                string url = "https://api.p2pquake.net/v2/history?codes=551";
+                HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+                if (responseMessage.IsSuccessStatusCode) {
+                    string jsonRaw = responseMessage.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject<List<P2PEarthquakeAPI>>(jsonRaw);
+                }
+            }
+            return null;
+        }
+
+        public EarthquakeInfo? earthquake { get; set; }
+        public EarthquakePoint[]? points { get; set; }
+
+        public class EarthquakeInfo {
+            public string domesticTsunami { get; set; }
+            public string time { get; set; }
+            public int maxScale { get; set; }
+            public Hypocenter hypocenter { get; set; }
+
+            public class Hypocenter {
+                public int depth { get; set; }
+                public float latitude { get; set; }
+                public float longitude { get; set; }
+                public float magnitude { get; set; }
+                public string name { get; set; }
+            }
+        }
+
+        public class EarthquakePoint {
+            public string addr;
+            public string pref;
+            public int scale;
+        }
+
+    }
+}
