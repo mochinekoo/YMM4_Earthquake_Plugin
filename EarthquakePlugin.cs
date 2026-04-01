b012583492494c78a0e2cb35a0aa7559219a4a90
mochinekoo <mochinekoo0113@gmail.com>
2026-04-01 19:34:39 +0900

    [add] ウインドウが出るようになりました

---
+++ EarthquakePlugin.cs
@@ -0,0 +1,9 @@
+﻿using YukkuriMovieMaker.Plugin;
+
+namespace YMM4_Earthquake_Plugin {
+    public class EarthquakePlugin : IToolPlugin {
+        public Type ViewModelType => typeof(MainViewModel);
+        public Type ViewType => typeof(MainEarthquakeWindow);
+        public string Name => "地震プラグイン";
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YMM4_Earthquake_Plugin {
+    internal class MainViewModel {
+    }
+}
