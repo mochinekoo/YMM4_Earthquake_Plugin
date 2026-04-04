de11d41e9438ffb23bc12c7e4053ae83e2d0adbb
mochinekoo <mochinekoo0113@gmail.com>
2026-04-04 12:41:25 +0900

    [add] プラグインの設定ビューを追加

---
+++ PluginSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YukkuriMovieMaker.Plugin;
+
+namespace YMM4_Earthquake_Plugin {
+    internal class PluginSettings : SettingsBase<PluginSettings> {
+
+        public override SettingsCategory Category => SettingsCategory.Tool;
+
+        public override string Name => "地震プラグイン";
+
+        public override bool HasSettingView => true;
+
+        public override object? SettingView {
+            get {
+                PluginSettingView pluginSettingView = new PluginSettingView();
+                return pluginSettingView;
+            }
+        }
+
+        public override void Initialize() {
+
+        }
+    }
+}
